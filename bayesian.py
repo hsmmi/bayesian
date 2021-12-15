86ff1ac7f940ec40b65faff8ff88c92ddc26a2bb
Hesam Mousavi <hesoyam@Hesams-MacBook-Pro.local>
2021-12-15 09:46:04 +0330

    get accuracy test in GLDA

--- bayesian.py
+++ bayesian.py
@@ -15,6 +15,12 @@ class Bayesian:
         self.number_of_sample = self.X.shape[0]
         self.number_of_feature = self.X.shape[1]
         self.number_of_class = self.diffrent_label.shape[0]
+        self.mean_class = None
+        self.covariance = None
+        self.probabilities = None
+        self.predicted_label = None
+        self.accuracy = None
+        self.test_accuracy = None
 
     def find_mean_class_i(self, label):
         '''
@@ -46,15 +52,48 @@ class Bayesian:
         self.phi = self.count_diffrent_label / self.number_of_sample
         self.mean_class = self.find_mean_classes()
         self.covariance = self.find_covariance()
-        print('hi')
 
-    def probabilities(self):
-        probabilities = np.zeros((self.number_of_sample, self.number_of_class))
-        coefficient 
-        
-        print('hi')
+    def find_probabilities(self, X_input=None):
+        if X_input is None:
+            X_input = self.X
+        number_of_sample = X_input.shape[0]
+        probabilities = np.zeros((number_of_sample, self.number_of_class))
+        coefficient = 1  # It's constant and doesn't change argmax
+        covariance_inverse = np.linalg.inv(self.covariance)
+        for label in self.diffrent_label.astype(int):
+            X_demean = X_input - self.mean_class[label]
+            P_X_given_y = coefficient * -0.5 * (
+                    ((X_demean @ covariance_inverse) * X_demean)
+                    @ np.ones((self.number_of_feature, 1)))
+            prior = self.phi[label]
+            probabilities[:, label:label+1] = P_X_given_y + prior
+        return probabilities
 
-b = Bayesian('dataset/BC-Train1.csv')
-b.find_parameters()
-b.probabilities()
-print('hi')
+    def find_prediction(self, probabilities=None):
+        if probabilities is None:
+            probabilities = self.probabilities
+        return np.argmax(probabilities, axis=1).reshape(-1, 1)
+
+    def find_accuracy(self, y_input=None, predicted_label=None):
+        if predicted_label is None:
+            predicted_label = self.predicted_label
+        if y_input is None:
+            y_input = self.y
+        return sum(y_input == predicted_label) / y_input.shape[0]
+
+    def runner(self):
+        self.find_parameters()
+        self.probabilities = self.find_probabilities()
+        self.predicted_label = self.find_prediction()
+        self.accuracy = self.find_accuracy()
+
+    def find_test_accuracy(self, file, normalization_method=None):
+        X_test, y_test = read_dataset_to_X_and_y(
+          file, normalization=normalization_method)
+        probabilities = self.find_probabilities(X_test)
+        predicted_label = self.find_prediction(probabilities)
+        self.test_accuracy = self.find_accuracy(y_test, predicted_label)
+        return self.test_accuracy
+
+
+
--- spr_hw_03.py
+++ spr_hw_03.py
@@ -1,9 +1,19 @@
+from bayesian import Bayesian
 from preprocessing import read_dataset_to_X_and_y
 
 # def linear_discriminant_analysis_runner ():
 X_train, y_train = read_dataset_to_X_and_y('dataset/BC-Train1')
 X_test, y_test = read_dataset_to_X_and_y('dataset/BC-Test1')
 
-# linear_discriminant_analysis_runner() # uncomment to run LDA
-print('hi')
+# linear_discriminant_analysis_runner() # uncomment to run GLDA
+GLDA1 = Bayesian('dataset/BC-Train1.csv')
+GLDA1.runner()
+GLDA1.find_test_accuracy('dataset/BC-Test1.csv')
+print(GLDA1.accuracy)
+print(GLDA1.test_accuracy)
+GLDA2 = Bayesian('dataset/BC-Train2.csv')
+GLDA2.runner()
+GLDA2.find_test_accuracy('dataset/BC-Test2.csv')
+print(GLDA2.accuracy)
+print(GLDA2.test_accuracy)
 # This is a new line that ends the file.
